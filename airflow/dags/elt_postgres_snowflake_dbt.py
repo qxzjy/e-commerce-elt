a7120f67a7fa153825d9e5d67c5c1b45e5e626a0
Maxime RENAULT <maxime.renault13@gmail.com>
2025-12-19 16:44:14 +0100

    feat(dags): add load to Snowflake task, update the deprecated Airflow Variable import, add task id to extract_to_parquet and load_to_s3 tasks, refactor tasks to use TaskFlowAPI to push data in XCom

--- airflow/dags/elt_postgres_snowflake_dbt.py
+++ airflow/dags/elt_postgres_snowflake_dbt.py
@@ -2,11 +2,13 @@ import logging
 from datetime import datetime
 
 import pandas as pd
-from airflow.models import Variable
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.providers.snowflake.transfers.copy_into_snowflake import (
+    CopyFromExternalStageToSnowflakeOperator,
+)
 from airflow.providers.standard.operators.empty import EmptyOperator
-from airflow.sdk import dag, task, task_group
+from airflow.sdk import Variable, dag, task, task_group
 
 logger = logging.getLogger(__name__)
 
@@ -16,20 +18,19 @@ TABLES = ["customers"]
 @dag(dag_id="elt-postgres-snowflake-dbt", start_date=datetime(2025, 12, 11), schedule=None, catchup=False)
 def elt_postgres_snowflake_dbt():
     
-    @task
-    def _extract_to_parquet(table_name: str, **context: dict) -> None:
+    @task(task_id='extract_to_csv', multiple_outputs=True)
+    def _extract_to_csv(table_name: str, **context: dict) -> dict:
         """
-        Extract data from a PostgreSQL table and save it as a Parquet file.
+        Extract data from a PostgreSQL table and save it as a CSV file.
         
         This task retrieves all data from a specified PostgreSQL table,
-        converts it to a Parquet format, and stores it in the /tmp directory.
-        The filename is pushed to XCom for downstream tasks.
+        converts it to a CSV format, and stores it in the /tmp directory.
         
         :param table_name: str - Name of the PostgreSQL table to extract
         :param context: dict - Airflow task context
         :raises Exception: If database connection fails or data processing errors occur
         
-        :returns: None - None
+        :returns: dict - Filename is pushed to XCom for downstream tasks.
         """
         try:
             postgres_hook = PostgresHook(postgres_conn_id="postgres_default")
@@ -39,38 +40,37 @@ def elt_postgres_snowflake_dbt():
             data = pd.read_sql(stmt, engine)
             data.set_index(data.columns[0], inplace=True)
             
-            parquet_file_name = f"{table_name}_{datetime.now().strftime('%Y%m%d-%H%M%S')}.parquet"
-            data.to_parquet(f"/tmp/{parquet_file_name}")
+            csv_file_name = f"{table_name}_{datetime.now().strftime('%Y%m%d-%H%M%S')}.csv"
+            data.to_csv(f"/tmp/{csv_file_name}")
             
-            context["task_instance"].xcom_push(key="parquet_file_name", value=parquet_file_name)
-            
-            logger.info(f"Successfully saved parquet file : {parquet_file_name}")
+            logger.info(f"Successfully saved csv file : {csv_file_name}")
         except Exception as e:
             logger.error(f"Error extracting data from {table_name}: {str(e)}")
-            raise    
+            raise
         
-    @task
+        return {"csv_file_name": csv_file_name}
+        
+    @task(task_id='load_to_s3', multiple_outputs=True)
     def _load_to_s3(table_name: str, **context: dict) -> None:
         """
-        Load data from a Parquet file to a AWS S3 bucket.
+        Load data from a CSV file to a AWS S3 bucket.
         
-        This task retrieves all data from a specified Parquet file,
+        This task retrieves all data from a specified CSV file,
         and load it into a AWS S3 bucket.
-        The filename is pulled from XCom from upstream tasks.
         
-        :param table_name: str - Name of the Parquet file to load
+        :param table_name: str - Name of the CSV file to load
         :param context: dict - Airflow task context
         :raises Exception: If database connection fails or data processing errors occur
         
         :returns: None - None
         """
         try:
-            parquet_file_name = context["task_instance"].xcom_pull(task_ids=f"extract_load.{table_name}_table._extract_to_parquet", key="parquet_file_name")
+            csv_file_name = context["task_instance"].xcom_pull(task_ids=f"extract_load.{table_name}_table.extract_to_csv", key="csv_file_name")
             
             s3_hook = S3Hook(aws_conn_id="aws_default")
-            s3_hook.load_file(filename=f"/tmp/{parquet_file_name}", key=f"data/{parquet_file_name}", bucket_name=Variable.get("S3_BUCKET_NAME"))
+            s3_hook.load_file(filename=f"/tmp/{csv_file_name}", key=f"data/{csv_file_name}", bucket_name=Variable.get("S3_BUCKET_NAME"))
             
-            logger.info(f"Successfully load parquet file into AWS S3 : s3://{Variable.get('S3_BUCKET_NAME')}/data/{parquet_file_name}")
+            logger.info(f"Successfully load csv file into AWS S3 : s3://{Variable.get('S3_BUCKET_NAME')}/data/{csv_file_name}")
         except Exception as e:
             logger.error(f"Error loading data into AWS S3 from {table_name}: {str(e)}")
             raise
@@ -86,10 +86,20 @@ def elt_postgres_snowflake_dbt():
         
         :returns: None - None
         """
-        extract_to_parquet = _extract_to_parquet(table_name)
+        extract_to_csv = _extract_to_csv(table_name)
         load_to_s3 = _load_to_s3(table_name)
         
-        extract_to_parquet >> load_to_s3
+        load_to_snowflake = CopyFromExternalStageToSnowflakeOperator(
+            task_id = "load_to_snowflake",
+            snowflake_conn_id = "snowflake_default",
+            files = [extract_to_csv["csv_file_name"]],
+            table = table_name,
+            schema = Variable.get("SNOWFLAKE_SCHEMA_RAW"),
+            stage = Variable.get("SNOWFLAKE_STAGE"),
+            file_format="CSV_FORMAT"
+        )
+        
+        extract_to_csv >> load_to_s3 >> load_to_snowflake
     
     @task_group(group_id='extract_load')
     def extract_load_group() -> None:
@@ -103,7 +113,7 @@ def elt_postgres_snowflake_dbt():
         for table_name in TABLES:
             
             table_group.override(group_id=f'{table_name}_table')(table_name)
-    
+            
     start = EmptyOperator(task_id='start')
     extract_load = extract_load_group()
     end = EmptyOperator(task_id='end')
