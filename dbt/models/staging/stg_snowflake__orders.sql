534096b0e22561726f21b570969bd41627b18f9a
Maxime RENAULT <maxime.renault13@gmail.com>
2025-12-23 22:10:47 +0100

    feat(dbt): update orders model, add CTE, cast date, create new category, add tests

--- dbt/models/staging/stg_snowflake__orders.sql
+++ dbt/models/staging/stg_snowflake__orders.sql
@@ -1,6 +1,22 @@
-SELECT
-    order_id,
-    customer_id,
-    order_date,
-    status
-FROM {{ source('snowflake_e_commerce', 'orders') }}
+with
+
+orders as (
+    select *
+    from {{ source('snowflake_e_commerce', 'orders') }}
+),
+
+orders_staged as (
+    select
+        order_id,
+        customer_id,
+        order_date::date as order_date,
+        status,
+        case
+            when status = 'completed' then true
+            else false
+        end as is_completed_order
+    from orders
+)
+
+select *
+from orders_staged
