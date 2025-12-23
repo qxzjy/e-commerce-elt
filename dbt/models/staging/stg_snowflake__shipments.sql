94a744138b2dcb4fd33a596ce425539c16b64434
Maxime RENAULT <maxime.renault13@gmail.com>
2025-12-23 22:16:42 +0100

    feat(dbt): update shipments model, add CTE, cast date

--- dbt/models/staging/stg_snowflake__shipments.sql
+++ dbt/models/staging/stg_snowflake__shipments.sql
@@ -1,7 +1,19 @@
-SELECT
-    shipment_id,
-    order_id,
-    shipped_date,
-    delivery_date,
-    carrier
-FROM {{ source('snowflake_e_commerce', 'shipments') }}
+with
+
+shipments as (
+    select *
+    from {{ source('snowflake_e_commerce', 'shipments') }}
+),
+
+shipments_staged as (
+    select
+        shipment_id,
+        order_id,
+        shipped_date::date as shipped_date,
+        delivery_date::date as delivery_date,
+        carrier
+    from shipments
+)
+
+select *
+from shipments_staged
