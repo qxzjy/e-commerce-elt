4e69f0e705f54f8736dc4e354ca0f5f4e847080d
Maxime RENAULT <maxime.renault13@gmail.com>
2025-12-23 21:58:22 +0100

    feat(dbt): update order_items model, add CTE

--- dbt/models/staging/stg_snowflake__order_items.sql
+++ dbt/models/staging/stg_snowflake__order_items.sql
@@ -1,6 +1,9 @@
-SELECT
-    item_id,
-    order_id,
-    product_id,
-    quantity
-FROM {{ source('snowflake_e_commerce', 'order_items') }}
+with
+
+order_items as (
+    select *
+    from {{ source('snowflake_e_commerce', 'order_items') }}
+)
+
+select *
+from order_items
