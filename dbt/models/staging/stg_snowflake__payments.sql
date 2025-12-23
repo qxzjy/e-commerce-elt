f4db8233ed2eaa43565dc647ef379509f222a403
Maxime RENAULT <maxime.renault13@gmail.com>
2025-12-23 22:12:01 +0100

    feat(dbt): update payments model, add CTE, cast date, create new category, add tests

--- dbt/models/staging/stg_snowflake__payments.sql
+++ dbt/models/staging/stg_snowflake__payments.sql
@@ -1,7 +1,23 @@
-SELECT
-    payment_id,
-    order_id,
-    amount,
-    payment_method,
-    payment_date
-FROM {{ source('snowflake_e_commerce', 'payments') }}
+with
+
+payments as (
+    select *
+    from {{ source('snowflake_e_commerce', 'payments') }}
+),
+
+payments_staged as (
+    select
+        payment_id,
+        order_id,
+        amount,
+        payment_method,
+        case
+            when payment_method = 'bank_transfer' then 'delayed'
+            else 'instant'
+        end as payment_method_type,
+        payment_date::date as payment_date
+    from payments
+)
+
+select *
+from payments_staged
