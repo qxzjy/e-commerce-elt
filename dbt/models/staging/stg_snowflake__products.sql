82b22989f734925e6e244a152109354e2cf35c91
Maxime RENAULT <maxime.renault13@gmail.com>
2025-12-23 22:13:55 +0100

    feat(dbt): update products model, add CTE, create new category, add tests

--- dbt/models/staging/stg_snowflake__products.sql
+++ dbt/models/staging/stg_snowflake__products.sql
@@ -1,6 +1,23 @@
-SELECT
-    product_id,
-    name,
-    price,
-    category
-FROM {{ source('snowflake_e_commerce', 'products') }}
+with
+
+products as (
+    select *
+    from {{ source('snowflake_e_commerce', 'products') }}
+),
+
+products_staged as (
+    select
+        product_id,
+        name,
+        price,
+        case
+            when price <= 75 then '0 - 75'
+            when price <= 150 then '75 - 150'
+            else '+150'
+        end as price_range,
+        category
+    from products
+)
+
+select *
+from products_staged
