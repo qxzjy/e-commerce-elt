71c17e9e16fe05982b442155e9e9ce329d7a8b26
Maxime RENAULT <maxime.renault13@gmail.com>
2026-01-10 15:32:27 +0100

    feat(dbt): add product dimension

---
+++ dbt/models/marts/dim_product.sql
@@ -0,0 +1,20 @@
+with
+
+products as (
+    select *
+    from {{ ref('stg_products') }}
+),
+
+dim_product as (
+    select
+        {{ dbt_utils.generate_surrogate_key(['product_id']) }} as product_sk,
+        product_id,
+        name,
+        category,
+        price,
+        price_range
+    from products
+)
+
+select *
+from dim_product
