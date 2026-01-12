6ab7acd687ebd5c200190bd8b4bb9403bfce9410
Maxime RENAULT <maxime.renault13@gmail.com>
2026-01-12 11:02:24 +0100

    feat(dbt): update order items enriched model, add status and date from orders, delete product name and category

--- dbt/models/intermediate/int_order_items_enriched.sql
+++ dbt/models/intermediate/int_order_items_enriched.sql
@@ -1,25 +1,44 @@
 with
 
 order_items as (
-    select * from {{ ref('stg_order_items') }}
+    select
+        item_id,
+        order_id,
+        product_id,
+        quantity
+    from {{ ref('stg_order_items') }}
+),
+
+orders as (
+    select
+        order_id,
+        customer_id,
+        order_date,
+        status as order_status
+    from {{ ref('stg_orders') }}
 ),
 
 products as (
-    select * from {{ ref('stg_products') }}
+    select
+        product_id,
+        price as unit_price
+    from {{ ref('stg_products') }}
 ),
 
 order_items_enriched as (
     select
-        o.item_id,
-        o.order_id,
-        p.product_id,
-        p.name as product_name,
-        p.category as product_category,
-        o.quantity,
-        p.price as product_unit_price,
-        o.quantity * p.price as total_price
-    from order_items o
-    left join products p on o.product_id = p.product_id
+        oi.item_id as order_item_id,
+        oi.order_id,
+        o.customer_id,
+        oi.product_id,
+        o.order_date,
+        o.order_status,
+        oi.quantity,
+        p.unit_price,
+        oi.quantity * p.unit_price as gross_amount
+    from order_items oi
+    left join orders o on oi.order_id = o.order_id
+    left join products p on oi.product_id = p.product_id
 )
 
 select * from order_items_enriched
