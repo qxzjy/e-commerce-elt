57ea603bb80b178cf208209f46585b46a4174c67
Maxime RENAULT <maxime.renault13@gmail.com>
2026-01-14 09:57:45 +0100

    feat(dbt): add surogate key to the date dimension

--- dbt/models/marts/dimension/dim_date.sql
+++ dbt/models/marts/dimension/dim_date.sql
@@ -1,1 +1,15 @@
-{{ dbt_date.get_date_dimension('2024-01-01', '2026-12-31') }}
+with 
+
+dim_date_raw as (
+    {{ dbt_date.get_date_dimension('2024-01-01', '2026-12-31') }}
+),
+
+dim_date as (
+    select
+        {{ dbt_utils.generate_surrogate_key(['date_day']) }} as date_sk,
+        dim_date_raw.*
+    from dim_date_raw
+)
+
+select *
+from dim_date
